8019ce5f61f1442eec5b37e4a50782378fadb2f8
BugFixDotExe <bugfix.exe@gmail.com>
2024-12-26 15:08:43 +0100

    debugging

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -12,57 +12,98 @@ class FilesController {
   static async postUpload(req, res) {
     const validTypes = ['folder', 'file', 'image'];
     const xTokenPayload = req.headers['x-token'];
-    console.log(xTokenPayload);
+
     try {
       const isValidKey = `auth_${xTokenPayload}`;
       const fetchDataFromRedis = await redisClient.get(isValidKey);
+      if (!fetchDataFromRedis) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
       const userCollection = await dbClient.client.db().collection('users');
       const isUser = await userCollection.findOne({ _id: ObjectId(fetchDataFromRedis) });
+      if (!isUser) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
       const userId = isUser._id;
+
       let { parentId, isPublic } = req.body;
       const { name, data, type } = req.body;
-      if (!parentId) { parentId = 0; }
-      if (!isPublic) { isPublic = false; }
-      if (!name) { return res.status(400).json({ error: 'Missing name' }); }
-      if (!validTypes.includes(type)) { return res.status(400).json({ error: 'Missing type' }); }
-      if (!data && type !== 'folder') { return res.status(400).json({ error: 'Missing data' }); }
+
+      if (!parentId) {
+        parentId = 0;
+      }
+      if (!isPublic) {
+        isPublic = false;
+      }
+
+      if (!name) {
+        return res.status(400).json({ error: 'Missing name' });
+      }
+      if (!validTypes.includes(type)) {
+        return res.status(400).json({ error: 'Missing type' });
+      }
+      if (!data && type !== 'folder') {
+        return res.status(400).json({ error: 'Missing data' });
+      }
+
+      if (parentId !== 0) {
+        const filesCollection = await dbClient.client.db().collection('files');
+        const parentFile = await filesCollection.findOne({ _id: ObjectId(parentId) });
+
+        if (!parentFile) {
+          return res.status(400).json({ error: 'Parent not found' });
+        }
+        if (parentFile.type !== 'folder') {
+          return res.status(400).json({ error: 'Parent is not a folder' });
+        }
+      }
 
       if (type === 'folder') {
         try {
           const filesCollection = await dbClient.client.db().collection('files');
-          const savedFile = await filesCollection.insertOne(
-            {
-              userId, name, type, isPublic, parentId,
-            },
-          );
+          const savedFile = await filesCollection.insertOne({
+            userId, name, type, isPublic, parentId,
+          });
           const savedPayload = savedFile.ops[0];
           res.status(201).json(savedPayload);
-        } catch (err) { console.log(err); return res.status(400).json({ error: err }); }
-      }
-      if (type === 'file' || type === 'image') {
+        } catch (err) {
+          console.log(err);
+          return res.status(400).json({ error: err });
+        }
+      } else if (type === 'file' || type === 'image') {
         const filesCollection = await dbClient.client.db().collection('files');
         const folder = process.env.FOLDER_PATH || '/tmp/files_manager';
 
         try {
-          if (!fs.existsSync(folder)) { fs.mkdirSync(folder); }
-        } catch (err) { console.log('From creating a dir: ', err); }
+          if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder);
+          }
+        } catch (err) {
+          console.log('From creating a dir: ', err);
+        }
 
         const localPath = uuid.v4();
         const cleanData = Buffer.from(data, 'base64').toString('utf8');
         const absolutePath = path.join(folder, localPath);
 
         fs.writeFile(absolutePath, cleanData, (err) => {
-          if (err) { console.log(err); }
+          if (err) {
+            console.log(err);
+          }
         });
-        const savedFile = await filesCollection.insertOne(
-          {
-            userId, name, type, isPublic, parentId, localPath: absolutePath,
-          },
-        );
+
+        const savedFile = await filesCollection.insertOne({
+          userId, name, type, isPublic, parentId, localPath: absolutePath,
+        });
         const savedPayload = savedFile.ops[0];
         res.status(201).json(savedPayload);
       }
-    } catch (error) { console.log(error); return res.status(401).json({ error: 'Unauthorized' }); }
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
   }
 }
+
 export default FilesController;
